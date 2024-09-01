0489005ac9cfc58dfdccf01b901bcf938939ef1e
david.zeleniak <david.zeleniak@e-o.solutions>
2024-09-01 09:26:30 -0700

    init

---
+++ lua/core/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.autowrite = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.timeoutlen = 500
+vim.opt.updatetime = 50
