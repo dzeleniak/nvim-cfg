f2428bf3abae5ba7771d07b98269444d2157a684
david.zeleniak <david.zeleniak@e-o.solutions>
2024-11-19 07:46:13 -0800

    lots of new toys

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.opt.timeoutlen = 500
+vim.opt.timeoutlen = 2000
 vim.opt.updatetime = 50
 
-vim.opt.tags = '.tags'
+vim.opt.tags = ".tags"
