5d339f4edc1aa15fd826a25cb63a41b7d747be0a
david.zeleniak <david.zeleniak@e-o.solutions>
2025-01-08 07:43:41 -0800

    install TS markdown

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,44 +1,46 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "elixir",
-          "javascript",
-          "html",
-          "ruby",
-          "python",
-        },
-        sync_install = false,
-        auto_install = true,
-        ignore_install = {},
-        modules = {
-          "highlight",
-          "incremental_selection",
-          "indent",
-          "rainbow",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-  {
-    -- Show delimiters in alternating colors
-    "HiPhish/rainbow-delimiters.nvim",
-  },
-  {
-    -- Show code context (fixing scope lines to the top)
-    "nvim-treesitter/nvim-treesitter-context",
-  },
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"elixir",
+					"javascript",
+					"html",
+					"ruby",
+					"python",
+					"markdown",
+					"markdown_inline",
+				},
+				sync_install = false,
+				auto_install = true,
+				ignore_install = {},
+				modules = {
+					"highlight",
+					"incremental_selection",
+					"indent",
+					"rainbow",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		-- Show delimiters in alternating colors
+		"HiPhish/rainbow-delimiters.nvim",
+	},
+	{
+		-- Show code context (fixing scope lines to the top)
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 }
